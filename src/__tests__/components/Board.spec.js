8cdba4964420bcdbaf79113de508d9feea000d52
Carlos Vargas <cornielleandres@gmail.com>
2018-11-15 10:40:26 -0500

    Remove unused shallow import from tests.

--- src/__tests__/components/Board.spec.js
+++ src/__tests__/components/Board.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 
 // Component
 import { Board, StyledBoard, HighScore, Top10 } from '../../components/index.js';
