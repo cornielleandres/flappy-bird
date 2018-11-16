d2e38175eda139ab9f19219b53340d984624b286
Carlos Vargas <cornielleandres@gmail.com>
2018-11-16 12:37:16 -0500

    Remove unused var.

--- src/__tests__/components/HighScore.spec.js
+++ src/__tests__/components/HighScore.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 
 // Component
 import { HighScore, StyledHighScore } from '../../components/index.js';
